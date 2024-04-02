8f29c46a5c23675608c1f70ffe01261b22eaecf9
Flips69 <aion.lipe@gmail.com>
2024-04-02 15:26:10 -0300

    02/04

--- src/screens/CadastroProduto.tsx
+++ src/screens/CadastroProduto.tsx
@@ -1,5 +1,7 @@
+
 import React, { useState } from "react";
 import { Image, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { launchCamera } from "react-native-image-picker";
 
 const CadastroProduto: React.FC = () => {
      const [produtos, setProdutos] = useState<Produto[]>([]);
@@ -9,6 +11,29 @@ const CadastroProduto: React.FC = () => {
      const [imagem, setImagem] = useState<any>('');
 
      const cadastrarProduto = async () => {
+
+     }
+
+     const abrirCamera = () => {
+        const options ={
+            mediaType: 'photo',
+            includeBase64: false,
+            maxHeight: 2000,
+            maxWidht: 2000
+        };
+        
+        launchCamera(options, response => {
+            if(response.didCancel){
+                console.log('cancelado pelo usuario');
+            } else if (response.error){
+                console.log('erro ao abrir a camera');
+            } else {
+                let imageUri = response.uri || response.assets?.[0]?.uri;
+                setImagem(imageUri);
+                console.log(imageUri);
+            }
+
+        })
 
      }
 
